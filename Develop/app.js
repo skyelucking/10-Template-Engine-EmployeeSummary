32a17c15b2e5103d89ac3e9eec68edf6ac4a241c
Skye Lucking <lucking.skye@gmail.com>
2021-01-27 16:43:43 -0700

    all tests a go!

--- Develop/app.js
+++ Develop/app.js
@@ -4,15 +4,82 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
+const util = require('util');
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./lib/htmlRenderer");
 
+ let Employees = []; 
+ 
+ const promptUser = () => {
+    return inquirer.prompt([
+      {
+          type: 'input',
+          name: 'id',
+          message: 'Please enter employee ID:',
+        },
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Please enter employee name:',
+      },
+      {
+          type: 'input',
+          name: 'email',
+          message: 'Please enter employee email:',
+        },  
+        {
+            type: 'list',
+            name: 'role',
+            message: 'Which type of employee do you want to add?',
+            choices: [
+                "Engineer",
+                "Intern",
+                "Manager",
+                "Quit"
+            ]
+          },      
+    ]);
+  };
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+ 
+
+  //   Bonus using async/await and try/catch
+const init = async () => {
+    console.log("Let's Get This Read-Me Party Started! Please Answer the Following Questions...");
+    try {
+      const answers = await promptUser();
+  
+      const html = generateReadMe(answers);
+  
+      await writeFileAsync('team.html', html);
+  
+      console.log('Successfully wrote to Employee File');
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+const generateReadMe = (answers) =>
+`
+<div class="card employee-card">
+    <div class="card-header">
+        <h2 class="card-title">${answers.name}</h2>
+        <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i></h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">ID: ${answers.id}</li>
+            <li class="list-group-item">Email: <a href="mailto:${answers.email}">${answers.email}</a></li>
+            <li class="list-group-item">School: ${answers.school}</li>
+        </ul>
+    </div>
+</div>
+
+ `;
+
+
+
 
 // After the user has input all employees desired, call the `render` function (required
 // above) and pass in an array containing all employee objects; the `render` function will
@@ -21,15 +88,20 @@ const render = require("./lib/htmlRenderer");
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+
 
 // HINT: each employee type (manager, engineer, or intern) has slightly different
 // information; write your code to ask different questions via inquirer depending on
 // employee type.
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+const employeeNew = new Employee(100, 'Alice', 'test@test.com');
+
+  
+console.log('---Employee---');
+employeeNew.printInfo();
+employeeNew.getName();
+employeeNew.getId();
+employeeNew.getEmail();
+employeeNew.getRole();
+init();
+
--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,99 +1,34 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const util = require('util');
 
-const writeFileAsync = util.promisify(fs.writeFile);
-
-const promptUser = () => {
-    return inquirer.prompt([
-      {
-          type: 'input',
-          name: 'id',
-          message: 'Please enter employee ID:',
-        },
-      {
-        type: 'input',
-        name: 'name',
-        message: 'Please enter employee name:',
-      },
-      {
-          type: 'input',
-          name: 'email',
-          message: 'Please enter employee email:',
-        },       
-    ]);
-  };
-
- 
 // TODO: Write code to define and export the Employee class
 class Employee {
-    constructor(id, name, email, role) {
+    constructor(name, id, email) {
       this.id = id;
       this.name = name;
       this.email = email;
-      this.role = role;
+      
     }
-  
+   PrintInfo(){
+      console.log(`--- ${this.role} ---`);
+  }
     getName(){
-        console.log(`Employee Name: ${this.name} `);
-    }
+      console.log(`Name: ${this.name} `);
+      return this.name;
+  }
     getId(){
-        console.log(`Employee ID: ${this.id} `);
+      console.log(`ID: ${this.id} `);
+        return this.id;
     }
     getEmail(){
-        console.log(`Employee email address: ${this.email} `);
+      console.log(`Email: ${this.email} `);
+        return this.email;
     }
     getRole(){
-        this.role = 'Employee';
-        console.log(`Employee Role: ${this.role} `);
+     
+      console.log(`Role: Employee`);
+      return "Employee";
     }
 
-  }
+  };
 
-// Bonus using async/await and try/catch
-
-// const generateReadMe = (answers) =>
-// `
-// <div class="card employee-card">
-//     <div class="card-header">
-//         <h2 class="card-title">${answers.name}</h2>
-//         <h3 class="card-title"><i class="fas fa-user-graduate mr-2"></i></h3>
-//     </div>
-//     <div class="card-body">
-//         <ul class="list-group">
-//             <li class="list-group-item">ID: ${answers.id}</li>
-//             <li class="list-group-item">Email: <a href="mailto:${answers.email}">${answers.email}</a></li>
-//             <li class="list-group-item">School: ${answers.school}</li>
-//         </ul>
-//     </div>
-// </div>
-
-//  `;
-// Bonus using async/await and try/catch
-// const init = async () => {
-//     console.log("Let's Get This Read-Me Party Started! Please Answer the Following Questions...");
-//     try {
-//       const answers = await promptUser();
-  
-//       const html = generateReadMe(answers);
-  
-//       await writeFileAsync('Employee_Write.html', html);
-  
-//       console.log('Successfully wrote to Employee File');
-//     } catch (err) {
-//       console.log(err);
-//     }
-//   };
-
-// const employeeNew = new Employee(100, 'Alice', 'test@test.com');
-
-  
-// console.log('---Employee---');
-// employeeNew.printInfo();
-// employeeNew.getName();
-// employeeNew.getId();
-// employeeNew.getEmail();
-// employeeNew.getRole();
-// init();
-  module.exports = Employee;
+module.exports = Employee;
   
